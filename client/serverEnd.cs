bf7613f516d520ae02100532c331100c867292e1
xianling zhang <alchemxz@gmail.com>
2017-02-05 16:25:46 -0500

    Add files via upload

---
+++ client/serverEnd.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+
+public class serverEnd : MonoBehaviour
+{
+
+    WebSocketServer server;
+
+    void Start()
+    {
+        server = new WebSocketServer(8888);
+
+        server.AddWebSocketService<Echo>("/");
+        server.Start();
+
+    }
+
+    void OnDestroy()
+    {
+        server.Stop();
+        server = null;
+    }
+
+}
+
+public class Echo : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Sessions.Broadcast(e.Data);
+    }
+}
